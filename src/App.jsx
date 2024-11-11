569aa22014b8b9597dca4551210901c3519010f2
Vladimir Beliakov <ermegilius@gmail.com>
2024-11-11 20:51:14 +0200

    end of the day 11.11.2024. Make card message editing work

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css'
-import Card from './components/Card.jsx'
-import personsData from './data/persons.js' //you can rename it anyway here
 import { useState } from 'react'
+import personsData from './data/persons.js' //you can rename it anyway here
+import Card from './components/Card.jsx'
 import Button from './components/Button.jsx'
- 
+import './App.css'
 
 function App() {
 
@@ -15,36 +14,36 @@ function App() {
   }
 
   const handleClick = (id) => {
-    console.log('I was clicked', id);//here we use id of the clicked element (from line 38)
+    console.log(`I'm card No ${id}. I was clicked`);//here we use id of the clicked element
   }
 
   return (
     <>
       <header>
-        <p>Logo</p>
+        <h1>My demo app</h1>
         <Button
           text={isLoggedIn ? "Log out" : "Log in"}
           click={toggleLogIn}
         />
-        </header>
-        <main>
-          {isLoggedIn ? (
-            personsData.map((person)=>(
-              <Card 
-                key={person.id}
-                firstName={person.firstName}
-                lastName = {person.lastName}
-                message={person.message}
-                click={()=>handleClick(person.id)} //here we register (and send further) id of the clicked element, we will use it later
-              />))
-            ):(
-              <p>Log-in to see the cards</p>
-            )
-          }
-        </main>
-        <footer>Footer</footer>
+      </header>
+      <main>
+        {isLoggedIn ? (
+          personsData.map((person)=>(
+            <Card 
+              key={person.id}
+              firstName={person.firstName}
+              lastName = {person.lastName}
+              message={person.message}
+              click={()=>handleClick(person.id)} //here we register (and send further(higher)) id of the clicked element, we will use it later
+            />))
+          ):(
+            <p>Log-in to see the cards</p>
+          )
+        }
+      </main>
+      <footer>Footer</footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import Button from './Button.jsx'
 
-const Card = ({message,firstName,lastName, click}) => {
-    return (
-      <div className="card">
-        <p>message: {message}</p>
-        <p>first name: {firstName}</p>
-        <p>last name: {lastName}</p>
-        <Button text="Edit" click={click}/>
-      </div>
-    )
-  }
+const Card = ({firstName, lastName, message}) => {
+  const [messageValue, setMessageValue] = useState(message);
+  const [isEditing, setIsEditing] = useState(false);
 
-  export default Card;
+  const toggleEdit = () => setIsEditing((prevState)=>!prevState);
+  const handleChange = (e) => setMessageValue(e.target.value);
+
+  return (
+    <div className="card">
+      <p>first name: {firstName}</p>
+      <p>last name: {lastName}</p>
+      {isEditing ? (
+        <input type='text' value={messageValue} onChange={handleChange} />
+      ) : (
+        <p>message: {messageValue}</p>
+      )}
+      <Button 
+        text={isEditing ? "Save" : "Edit"}
+        click={toggleEdit}
+      />
+    </div>
+  );
+}
+
+export default Card;
